4e4b5e712b5dc37c9ede57e3b599e2bacda67ab5
Safari Germain <pzx115866@student.wsb.poznan.pl>
2023-09-06 19:24:04 +0200

    touch

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Maisha.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HELNET.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
