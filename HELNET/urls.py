a823b225b031697141dc3ec7e3caff6c93b236cd
Safari Germain <pzx115866@student.wsb.poznan.pl>
2023-09-08 13:21:08 +0200

    changed urls.py

--- HELNET/urls.py
+++ HELNET/urls.py
@@ -10,4 +10,4 @@ urlpatterns = [
     path('api/', include('base.api.urls'))
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
