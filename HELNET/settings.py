aad923485ef1fc0fe3fe6feeb481a37e18d50e25
Safari Germain <pzx115866@student.wsb.poznan.pl>
2025-05-26 21:47:25 +0200

    ready again for deployment

--- HELNET/settings.py
+++ HELNET/settings.py
@@ -9,15 +9,17 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import dj_database_url
+import os
 from pathlib import Path
+
+# Optional: Only import dj_database_url if DATABASE_URL is set (for production)
+try:
+    import dj_database_url
+except ImportError:
+    dj_database_url = None
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-3#av2c6nptlbbb6^muqkchu&fe3wv&n$t2+g$v!ir-f5%doocb'
@@ -26,7 +28,6 @@ SECRET_KEY = 'django-insecure-3#av2c6nptlbbb6^muqkchu&fe3wv&n$t2+g$v!ir-f5%doocb
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -83,20 +84,20 @@ WSGI_APPLICATION = 'HELNET.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+# Use SQLite by default for local development.
+# If the DATABASE_URL environment variable is set (e.g. in production), use it.
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-# postgres://movies_8k05_user:6TcEIG7nKEy9fv0P7YuBvS0h3xsCWEXV@dpg-cjfsmkvr2b1s73fvvvig-a.oregon-postgres.render.com/movies_8k05
-DATABASES['default'] = dj_database_url.parse(
-    'postgres://movies_8k05_user:6TcEIG7nKEy9fv0P7YuBvS0h3xsCWEXV@dpg-cjfsmkvr2b1s73fvvvig-a.oregon-postgres.render.com/movies_8k05')
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
+DATABASE_URL = os.environ.get("DATABASE_URL")
+if DATABASE_URL and dj_database_url:
+    DATABASES['default'] = dj_database_url.parse(DATABASE_URL)
+
+# Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -112,32 +113,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-# STATIC_URL = '/static/'
-# MEDIA_URL = '/images/'
-
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static'
-# ]
-
-# MEDIA_ROOT = BASE_DIR / 'static/images'
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
@@ -148,12 +131,6 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / 'collected_static'
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
-# STATIC_ROOT =
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 CORS_ALLOW_ALL_ORIGINS = True
